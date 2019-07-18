974eea8358e0f6a33d34cd5e10036ba37b41600a
brenodev <breno.souza.developer@gmail.com>
2019-07-18 17:36:15 -0300

    iniciado projeto

---
+++ src/main/Calculator/Calculator.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import "./Calculator.css";
+
+class Calculator extends Component {
+  render() {
+    return (
+      <div className="calculator">
+        <h1>ola mundo</h1>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
